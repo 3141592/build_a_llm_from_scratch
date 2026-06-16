aade172ccb38ca10eb283d004ef884c0d4d14cb3
Roy Gunter <roygunter@hushmail.com>
2026-06-16 01:11:27 +0000

    Add bash history

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -9,4 +9,26 @@ apt-get install -y tmux
 pip install -e /workspaces/ai_shared_utilities
 pip install black
 
-ln -s /usr/bin/python3 /usr/bin/python
+ln -sf /usr/bin/python3 /usr/bin/python
+
+cat > /root/.bashrc_devcontainer <<'EOF'
+# ---- Bash history ----
+
+export HISTSIZE=100000
+export HISTFILESIZE=200000
+export HISTCONTROL=ignoredups:erasedups
+
+shopt -s histappend
+
+# Save history immediately and import commands from other shells
+export PROMPT_COMMAND='history -a; history -n'
+
+# ---- Up/down arrow prefix search ----
+
+bind '"\e[A": history-search-backward'
+bind '"\e[B": history-search-forward'
+EOF
+
+grep -qxF 'source /root/.bashrc_devcontainer' /root/.bashrc \
+      || echo 'source /root/.bashrc_devcontainer' >> /root/.bashrc
+
